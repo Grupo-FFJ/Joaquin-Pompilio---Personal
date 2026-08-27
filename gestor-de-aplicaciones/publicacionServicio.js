f3ba32666d333e5af3ad3abb1fc00df77656628f
Joaquin <joacopompilio@gmail.com>
2026-08-27 14:34:10 -0300

    Parte 2.5 · Roles en las asociaciones / PRACTICO 5

--- gestor-de-aplicaciones/publicacionServicio.js
+++ gestor-de-aplicaciones/publicacionServicio.js
@@ -2,14 +2,19 @@ import { Publicacion } from "./publicacion.js";
 export class PublicacionServicio extends Publicacion {
     modalidad
     duracionMinutos
+    cliente
     //
-    constructor(titulo,descripcion,autor,modalidad,duracionMinutos){
+    constructor(titulo,descripcion,autor,modalidad,duracionMinutos,cliente = null){
         super(titulo,descripcion,autor);
         this.modalidad = modalidad
         this.duracionMinutos = duracionMinutos
+        this.cliente = cliente
     }
         mostrarResumen() {
         const resumenPadre = super.mostrarResumen()
         return `${resumenPadre} modalidad: ${this.modalidad} duracion: ${this.duracionMinutos}`
     }
-}
+}   /* En PublicacionServicio.js agreguen this.cliente, otro atributo del tipo Usuario que
+representa quién reservó el servicio. Ahora Usuario cumple dos roles distintos frente
+a Publicacion: autor y cliente. */
+    
--- gestor-de-aplicaciones/usuario.js
+++ gestor-de-aplicaciones/usuario.js
@@ -1,16 +1,18 @@
 export class Usuario{
-    //es para los types
-    #nombre
-    #email
-    #fechaRegistro
-    //
     constructor(nombre,email){
         this.nombre = nombre
         this.email = email
         this.fechaRegistro = new Date()
+        this.contacto = []
     }
     mostrarPerfil() {
         return this.nombre+" "+this.email
     }
    
 }
+/*¿Por qué this.contactos es una asociación y no una dependencia? ¿Qué multiplicidad tiene
+esa auto-asociación (Usuario 1 — * Usuario)?
+-Es una asociación porque la referencia a los otros objetos Usuario se almacena como un atributo en la clase this.contactos y se puede acceder en cualquier instancia  
+-Es una multiplicidad de uno a muchos un único usuario puede tener referenciados en su lista de contactos cero, uno o múltiples objetos de tipo Usuario
+|Usuario |1 — * |Usuario|
+*/
